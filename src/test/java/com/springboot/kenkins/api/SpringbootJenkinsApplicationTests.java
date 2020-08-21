4d9537e25b4e1ca137fe7d59c7236e5711e1cdf8
jazeem1992 <jazeem1992@gmail.com>
2020-08-21 19:32:41 +0530

    first commit

---
+++ src/test/java/com/springboot/kenkins/api/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.springboot.kenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringbootJenkinsApplicationTests.class);
+	
+	@Test
+	public void contextLoads() {
+		logger.info("Test case Executing....");
+		assertEquals(true, true);
+	}
+
+}
